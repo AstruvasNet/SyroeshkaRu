b8c338584be9a70f061dcdb226758a28583e050c
astruvas <astruvas@gmail.com>
2019-07-06 08:10:58 +0300

    COMMIT Удаление скомпилированных файлов

--- SyroeshkaRu.UserInterface/SYR.UserInterface.MVC/Client/js/components/store.component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var store_module_1 = require("../modules/store.module");
-var boot = new store_module_1.StoreModule.Boot();
-$(function () {
-    boot.scrollEvent(window);
-    boot.load();
-});
-//# sourceMappingURL=store.component.js.map
